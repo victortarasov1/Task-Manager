8778964bd88e8c583f4305239bdf4207bc1ffbdf
victortarasov1 <victortarasov13032001@gmail.com>
2024-04-25 12:27:18 -0700

    refactored exception handler

--- src/main/java/tarasov/victor/taskmanager/controller/GlobalExceptionHandler.java
+++ src/main/java/tarasov/victor/taskmanager/controller/GlobalExceptionHandler.java
@@ -9,15 +9,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tarasov.victor.taskmanager.dto.ErrorResponseDto;
 import tarasov.victor.taskmanager.exception.TaskManagerException;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
@@ -39,21 +35,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        Function<ObjectError, String> toFieldName = v -> ((FieldError) v).getField();
-        Function<ObjectError, String> toMessage = ObjectError::getDefaultMessage;
-        Predicate<ObjectError> withMessage = v -> v.getDefaultMessage() != null;
         var allErrors = ex.getBindingResult().getAllErrors();
-        var errorsInfo = allErrors.stream().filter(withMessage).collect(Collectors.toMap(toFieldName, toMessage, (a, b) -> a + ", " + b));
+        var errorsWithMessages = allErrors.stream().filter(v -> v.getDefaultMessage() != null);
+        var errorsInfo = errorsWithMessages.collect(Collectors.toMap(v -> ((FieldError) v).getField(),
+                ObjectError::getDefaultMessage, (a, b) -> a + ", " + b));
         return new ResponseEntity<>(errorsInfo, BAD_REQUEST);
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponseDto> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
-        var message = "The parameter " + ex.getName() + " of value '" + ex.getValue() + "' could not be converted to type "
-                + Objects.requireNonNull(ex.getRequiredType()).getSimpleName();
-        var error = new ErrorResponseDto(message, LocalDateTime.now());
-        return new ResponseEntity<>(error, BAD_REQUEST);
-
     }
 
 }
